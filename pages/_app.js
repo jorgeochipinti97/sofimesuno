b0ed80815cb2f65d2aaf0749eef62206da707e91
Jorge Ariel Ochipinti <jorge@Jorges-MacBook-Pro.local>
2023-08-18 04:46:37 -0300

    l

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { lightTheme } from "@/themes/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
